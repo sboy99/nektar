3b0386060cb0db53312a3b2ed357092f08702be9
sboy99 <shadowboy191@gmail.com>
2026-07-10 19:34:39 +0530

    chore: main.go for nektar

---
+++ cmd/nektar/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/sboy99/nektar/internal/bootstrap"
+	"github.com/sboy99/nektar/internal/platform/config"
+	"github.com/sboy99/nektar/internal/platform/logger"
+)
+
+func main() {
+	configPath := flag.String("config", "configs/config.yaml", "path to config file")
+	flag.Parse()
+
+	log := logger.New()
+
+	cfg, err := config.Load(*configPath)
+	if err != nil {
+		log.Error("failed to load config", "error", err)
+		os.Exit(1)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	app, err := bootstrap.Build(ctx, cfg, log)
+	if err != nil {
+		log.Error("failed to bootstrap application", "error", err)
+		os.Exit(1)
+	}
+
+	log.Info("nektar started",
+		"eventbus", cfg.EventBus.Provider,
+		"storage", cfg.Storage.Provider,
+		"cache", cfg.Cache.Provider,
+	)
+
+	if err := app.Run(ctx); err != nil {
+		log.Error("application stopped with error", "error", err)
+		os.Exit(1)
+	}
+
+	log.Info("nektar stopped")
+}
